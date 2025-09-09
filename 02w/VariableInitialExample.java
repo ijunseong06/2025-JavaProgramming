6b1e47d6f1dd2c90287d70285f615adbcc1278e8
DESKTOP-KUIA7C1\109-1 <dongyang@dongyang.ac.kr>
2025-09-09 11:02:14 +0900

    commit

--- 02w/VariableInitialExample.java
+++
@@ -1,9 +0,0 @@
-public class VariableInitialExample {
-    public static void main(String[] args) {
-        int value = 0;
-
-        int result = value + 10;
-
-        System.out.println(result);
-    }
-}
---
+++ 02w/src/VariableInitialExample.java
@@ -0,0 +1,9 @@
+public class VariableInitialExample {
+    public static void main(String[] args) {
+        int value = 0;
+
+        int result = value + 10;
+
+        System.out.println(result);
+    }
+}
